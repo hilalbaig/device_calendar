31a5a206f22697e69b255547ef9ac182020c7b02
Hafeez Ahmed <hahmed@fusionsd.com.au>
2024-11-26 13:38:29 +0500

    refactor: improve source filtering logic in SwiftDeviceCalendarPlugin

--- ios/Classes/SwiftDeviceCalendarPlugin.swift
+++ ios/Classes/SwiftDeviceCalendarPlugin.swift
@@ -199,20 +199,20 @@ public class SwiftDeviceCalendarPlugin: NSObject, FlutterPlugin, EKEventViewDele
     }
 
     private func getSource() -> EKSource? {
-      let localSources = eventStore.sources.filter { $0.sourceType == .local }
+        let localSources = eventStore.sources.filter { $0.sourceType == .local }
 
             if (!localSources.isEmpty) {
                 return localSources.first
             }
 
-            if let defaultSource = eventStore.defaultCalendarForNewEvents?.source {
-                return defaultSource
-            }
-
-            let iCloudSources = eventStore.sources.filter { $0.sourceType == .calDAV && $0.sourceIdentifier == "iCloud" }
+            let iCloudSources = eventStore.sources.filter { $0.sourceType == .calDAV && $0.title == "iCloud" }
 
             if (!iCloudSources.isEmpty) {
                 return iCloudSources.first
+            }
+
+            if let defaultSource = eventStore.defaultCalendarForNewEvents?.source {
+                return defaultSource
             }
 
             return nil
